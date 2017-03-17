dd9cffef69e03cc6b7143cb63b2d46be5df50821
zhangweiwei <zhangweiwei@jk.cn>
2017-03-17 10:42:49 +0800

    图

---
+++ src/数据结构与算法JavaScript描述/第 11 章 图和图算法/Graph.js
@@ -0,0 +1,119 @@
+/**
+ * 图
+ */
+
+function Graph() {
+    // 存储 顶点之间的关系
+    this.adj = {};
+
+    // 边的个数
+    this.edges = 0;
+
+    // 存储已访问过的顶点
+    this.marked = {};
+
+    // 路径 bfs 中使用
+    this.edgeTo = {};
+}
+
+// 新增边
+Graph.prototype.addEdge = function (v, w) {
+    this.adj[v] = this.adj[v] || [];
+    this.adj[v].push(w);
+
+    this.adj[w] = this.adj[w] || [];
+    this.adj[w].push(v);
+
+    this.edges++;
+};
+
+// 清除 marked
+Graph.prototype.clearMarked = function () {
+    this.marked = {};
+};
+
+// 清除 edgeTo
+Graph.prototype.clearEdgeTo = function () {
+    this.edgeTo = {};
+};
+
+// 深度优先搜索
+Graph.prototype.dfs = function (v) {
+    if (v == null) {
+        return;
+    }
+    this.marked[v] = true;
+
+    if (this.adj[v] != null) {
+        console.log('访问过的节点：' + v);
+    }
+
+    this.adj[v].forEach((w) => {
+        if (!this.marked[w]) {
+            this.dfs(w);
+        }
+    });
+};
+
+// 广度优先搜索
+Graph.prototype.bfs = function (v) {
+    if (v == null) {
+        return;
+    }
+    this.marked[v] = true;
+
+    // 待访问的队列
+    let queue = [];
+
+    queue.push(v);
+    while (queue.length) {
+        let w = queue.shift();
+
+        // 把未访问的加入队列中
+        this.adj[w].forEach((t) => {
+            if (!this.marked[t]) {
+                this.marked[t] = true;
+                this.edgeTo[t] = w;
+                queue.push(t);
+            }
+        });
+        
+        console.log('访问过的节点：' + w, this.edgeTo, this.adj);
+    }
+};
+
+// 路径
+// v 到 w 的路径
+Graph.prototype.pathTo = function (v, w) {
+    if (v == null || w == null) {
+        return;
+    }
+    let path = [w],
+        sideSize = this.edges;
+
+    // 循环不得大于边的个数
+    while (sideSize--) {
+        if (w === v) {
+            break;
+        }
+        w = this.edgeTo[w];
+        path.push(w);
+    }
+
+    // 判断路径是否正确
+    if (path[path.length - 1] === v) {
+        return path.reverse();
+    }
+
+    return null;
+};
+
+// 显示图
+Graph.prototype.display = function () {
+    Object.keys(this.adj).forEach((key) => {
+        console.log(key + ': ' + this.adj[key].toString());
+    });
+};
+
+
+export default {Graph};
---
+++ src/数据结构与算法JavaScript描述/第 11 章 图和图算法/exercise.js
@@ -0,0 +1,137 @@
+import {Graph} from './Graph';
+
+/**
+ * 1、编写一个程序，测试广度优先和深度优先这两种图搜索算法哪一种速度更快。请使用不同大小的图来测试你的程序。
+ */
+var g = new Graph();
+
+g.addEdge(0, 1);
+g.addEdge(0, 2);
+g.addEdge(1, 3);
+g.addEdge(2, 4);
+g.addEdge(0, 4);
+g.display();
+g.dfs(0);
+g.clearMarked();
+g.bfs(0);
+
+
+/**
+ * 4、构建一个图，用它为你居住地的地图建模。测试一下从一个开始顶点到最后顶点的最短路径。
+ */
+// 广度优先搜索
+var g4 = new Graph();
+
+g4.addEdge(0, 1);
+g4.addEdge(0, 2);
+g4.addEdge(0, 4);
+g4.addEdge(1, 3);
+g4.addEdge(2, 4);
+g4.display();
+g4.bfs(0);
+g4.pathTo(0, 4);
+
+
+/**
+ * 5、对上一题中创建的图执行深度优先搜索和广度优先搜索。
+ */
+// 深度优先搜索
+Graph.prototype.dfsMinHelper = function (v, end, path = [v]) {
+    if (v == null) {
+        return;
+    }
+    this.marked[v] = true;
+
+    path.push(v);
+    this.adj[v].forEach((w) => {
+        if (!this.marked[w]) {
+            if (v === end) {
+                return path;
+            }
+
+            this.dfsMinHelper(w, end, path);
+        }
+    });
+
+    return path;
+};
+Graph.prototype.dfsMin = function (v, end) {
+    var path = [];
+
+    this.adj[v].forEach((w) => {
+        this.clearMarked();
+        this.marked[v] = true;
+        let tempPath = this.dfsMinHelper(w, end, [v]);
+
+        if (tempPath[tempPath.length - 1] === end) {
+            path.push(tempPath);
+        }
+    });
+    console.log('全部路径：' + path);
+
+    let letMinPath;
+
+    // 最短路径
+    path.forEach((p) => {
+        if (!letMinPath) {
+            letMinPath = p;
+        } else if (letMinPath.length > p.length) {
+            letMinPath = p;
+        }
+    });
+
+    return letMinPath;
+};
+
+// 测试
+var g5 = new Graph();
+
+g5.addEdge(0, 4);
+g5.addEdge(0, 1);
+g5.addEdge(0, 2);
+g5.addEdge(1, 3);
+g5.addEdge(2, 4);
+g5.display();
+g5.dfsMin(0, 4);
+
+// 广度优先搜索
+Graph.prototype.bfsMin = function (v, end) {
+    var path = [];
+
+    this.adj[v].forEach((w) => {
+        this.clearMarked();
+        this.clearEdgeTo();
+        this.marked[v] = true;
+        this.bfs(w, end, [v]);
+        let tempPath = this.pathTo(w, end);
+
+        if (tempPath) {
+            path.push([v].concat(tempPath));
+        }
+    });
+    console.log('全部路径：' + path);
+
+    let letMinPath;
+
+    // 最短路径
+    path.forEach((p) => {
+        if (!letMinPath) {
+            letMinPath = p;
+        } else if (letMinPath.length > p.length) {
+            letMinPath = p;
+        }
+    });
+
+    return letMinPath;
+};
+
+// 测试
+var g52 = new Graph();
+
+g52.addEdge(0, 4);
+g52.addEdge(0, 1);
+g52.addEdge(0, 2);
+g52.addEdge(1, 3);
+g52.addEdge(2, 4);
+g52.display();
+g52.bfsMin(0, 4);
